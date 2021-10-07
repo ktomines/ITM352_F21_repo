8cbb279145909f6bcbfee67b2ec5c3085ab0a6f3
ktomines <ktomines@hawaii.edu>
2021-10-06 22:51:46 -1000

    Update Ex1.js

--- Lab8/Ex1.js
+++ Lab8/Ex1.js
@@ -1,14 +1,15 @@
 require("./products_data.js"); 
 
 var num_products = 5;
+var count = 0;
 
-var count = 1;
-while (count <= num_products) {
-    if (count > num_products/2) {
-        console.log("That's enough!");
-        break;
-    }
-    console.log(`${count}.${eval('name'+ count)}`);
+while (count < num_products) {
     count++;
+    if (count > num_products/4 && count < (num_products * 0.75)) {
+        console.log(`${eval('name' + count)} is sold out.`);
+    }
+    else {
+    console.log(`${count}.${eval('name'+ count)}`);
+    }
 }
 console.log("That's all we have folks!");
