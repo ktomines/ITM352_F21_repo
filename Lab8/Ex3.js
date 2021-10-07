5c15ba6c064aac9ba969c0e6953d04aee8aa14a8
ktomines <ktomines@hawaii.edu>
2021-10-06 22:52:04 -1000

    Create Ex3.js

---
+++ Lab8/Ex3.js
@@ -0,0 +1,6 @@
+require("./products_data.js"); 
+
+var num_products = 5;
+for (var count =1; eval("typeof name"+ count) != 'undefined'; count++){
+    console.log(`${count}.${eval('name' + count)}`);
+}
