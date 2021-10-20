b73f180341ba931c67fb1f666611d6f6a45d0d71
ktomines <ktomines@hawaii.edu>
2021-10-20 13:32:04 -1000

    Create Ex1.js

---
+++ Lab9/Ex1.js
@@ -0,0 +1,9 @@
+step1=21;
+step2=parseInt(step1/4);
+step3 = step2 + step1;
+step4 = 0; // Not Jan, so look at month before on table
+step6 = step4 + step3;
+step7 = step4 + step6;
+step9 = 1-step7;
+step10 = (step9/7)
+
