d162277b8927b2f7d1db164a3e94a45c5fda9d4f
ktomines <ktomines@hawaii.edu>
2021-10-21 01:02:23 -1000

    Update Ex1.js

--- Lab9/Ex1.js
+++ Lab9/Ex1.js
@@ -1,9 +1,24 @@
+date = 4;
+month = 10;
+year = 2001;
+
+
 step1=21;
 step2=parseInt(step1/4);
 step3 = step2 + step1;
-step4 = 0; // Not Jan, so look at month before on table
+step4 = 0; 
 step6 = step4 + step3;
-step7 = step4 + step6;
-step9 = 1-step7;
-step10 = (step9/7)
+step7 = step6 + date;
+step8 = step7 - 1;
+step9 = step8 % 7; 
 
+var weekDay=new Array(7);
+weekDay[0]="Sunday";
+weekDay[1]="Monday";
+weekDay[2]="Tuesday";
+weekDay[3]="Wednesday";
+weekDay[4]="Thursday";
+weekDay[5]="Friday";
+weekDay[6]="Saturday";
+
+console.log(`My bday is ${weekDay[step9]}.`);
