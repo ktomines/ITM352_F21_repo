caa5162b990c2c1595c27e8ba77aa14abf7cd516
ktomines <ktomines@hawaii.edu>
2021-11-18 00:54:44 -1000

    im dropping out

--- Krizel_Tomines_Assignment1/public/products.js
+++ Krizel_Tomines_Assignment1/public/products.js
@@ -10,26 +10,26 @@ var products =
     "brand": "Rabbit Signature",
     "price": 5.00,
     "image": "rbtsig.jpeg",
-    "quantity_available": 50
+    "quantity_available": 9
 },
 {
     "brand": "Lychee Green Tea",
     "price": 5.00,
     "image":  "lycheegreen.jpeg",
-    "quantity_available": 40
+    "quantity_available": 9
 },
 {
     "brand": "Tiramisu Milk Tea",
     "price": 4.00,
     "image":  "tiramisu.jpeg",
-    "quantity_available": 30
+    "quantity_available": 9
 },
 
 {
     "brand": "Queen of Hearts Mojito",
     "price": 5.00,
     "image":  "mojito.jpeg",
-    "quantity_available": 50
+    "quantity_available": 9
 }
 ]
 // an array of the products i would like to sell 
